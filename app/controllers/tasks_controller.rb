93b2ef925630254e583ff4a41ed960103bcaa086
Mohammed Raheem <mojoraheem@icloud.com>
2020-08-11 16:46:48 +0100

    on to update method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,36 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+    # redirects to the exact page we have just created using the create method.
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
